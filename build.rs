6fa1448bc2e70d753dce3a8ce3bccffdd2424d4a
lemolatoon <63438515+lemolatoon@users.noreply.github.com>
2023-05-27 00:43:33 +0900

    Use CXX with no_std

--- build.rs
+++ build.rs
@@ -1,8 +1,16 @@
 fn main() {
-    cxx_build::bridge("src/main.rs")
-        .file("cpp/test.cpp")
-        .compile("cxx-example");
+    //     const CXX_FLAGS: &str =
+    //         "-ffreestanding -mno-red-zone -fshort-wchar -fno-exceptions -fno-rtti -std=c++17 -L/usr/lib/x86_64-linux-gnu";
+    // const CXX_FLAGS: &str = "-std=c++17 -L/usr/lib/x86_64-linux-gnu";
+    // const CXX_FLAGS: &str = "";
+    let mut cc = cxx_build::bridge("src/main.rs");
+    // let mut cc = &mut cc;
+    // for flag in CXX_FLAGS.split_whitespace() {
+    //     cc = cc.flag(flag);
+    // }
+    cc.file("cpp/test.cpp").compile("cxx-example");
 
+    println!("cargo:rustc-link-lib=c");
     println!("cargo:rerun-if-changed=src/main.rs");
     println!("cargo:rerun-if-changed=cpp/test.cpp");
     println!("cargo:rerun-if-changed=cpp/include/test.hpp");
--- cpp/test.cpp
+++ cpp/test.cpp
@@ -1,22 +1,81 @@
 #include "include/test.hpp"
+#include "rust/cxx.h"
+#include <iostream>
 #include <memory>
 
-namespace test_class {
-TestClass::TestClass(int64_t value): value_(value) {}
-
-int64_t TestClass::getValue() const {
-    return value_;
+extern "C"
+{
+    void __cxa_atexit(){};
+    void __stack_chk_fail(){};
+    void _Unwind_Resume(){};
 }
 
-void TestClass::increment() {
-    value_++;
+namespace test_class
+{
+    TestClass::TestClass(int64_t value) : value_(value) {}
+
+    int64_t TestClass::getValue() const
+    {
+        return value_;
+    }
+
+    void TestClass::increment()
+    {
+        value_++;
+    }
+
+    void TestClass::increment_by(int64_t by)
+    {
+        value_ += by;
+    }
+
+    std::unique_ptr<TestClass> new_test_class(int64_t value)
+    {
+        return std::make_unique<TestClass>(value);
+    }
 }
 
-void TestClass::increment_by(int64_t by) {
-    value_ += by;
+namespace template_test
+{
+    template <typename T>
+    StackVector<T>::StackVector() : size(0) {}
+
+    template <typename T>
+    bool StackVector<T>::push(T value)
+    {
+        if (size == 20)
+        {
+            return false;
+        }
+        value_[size] = value;
+        size++;
+        return true;
+    }
+
+    template <typename T>
+    T StackVector<T>::pop()
+    {
+        if (size == 0)
+        {
+            return nullptr;
+        }
+        size--;
+        return value_[size];
+    }
+
+    template <typename T>
+    size_t StackVector<T>::len()
+    {
+        return size;
+    }
+
+    std::unique_ptr<StackVector<int32_t>> new_stack_vector()
+    {
+        return std::make_unique<StackVector<int32_t>>();
+    };
 }
 
-std::unique_ptr<TestClass> new_test_class(int64_t value) {
-    return std::make_unique<TestClass>(value);
-}
+void put(rust::Str s)
+{
+    std::cout << std::string(s);
 }
